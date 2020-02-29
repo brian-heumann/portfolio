c08211c292c09570bd7e9e271c3cce4707216791
Brian <brian.heumann@thinkregion.com>
2020-02-29 12:09:38 +0100

    F Allocation.

---
+++ shared/Allocation.py
@@ -0,0 +1,8 @@
+class Allocation:
+
+    def __init__(self, isin: str, target_alloc: float, pieces: float):
+        self.isin = isin
+        self.target_alloc = target_alloc
+        self.pieces = pieces
+
+    
